8302c02675923604630731be3e57e0beef2d4233
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-11-03 10:36:04 +0530

    Feat: add Vercel Analytics integration to the application

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { GameProvider } from "./context/GameContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <GameProvider>
       <App />
+      <Analytics />
     </GameProvider>
   </BrowserRouter>
 );
